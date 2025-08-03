734d22413637a4ad141bd028677e9af724319d82
miltondatta <gupta.miltondatta@gmail.com>
2025-08-03 14:08:33 +0600

    Final Commit

---
+++ config/constant.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'apikey' => [
+        'server_key' => 'AAAAMWLiD7c:APA91bHPEzoiT_XKfcxxliDqA7-CsM95GfHNqzvwT4dMa0JAYzW_wgwBJTXveFKjASxIaixHXe3hfI8fNmEt7L6gCa98eKQDkC2zKCBMdal72fU61TTeFfqckhz-BvrzVWlyXc3yR8-2'
+      ],
+];
